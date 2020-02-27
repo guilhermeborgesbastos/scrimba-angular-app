262921ef4b9cf45cf070db4ad915b4e4b9a3f336
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-02-27 20:31:47 -0300

    Added the DataService using Observable(subscribe) from rxjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CustomersModule } from './customers/customers.molule'
 import { SharedModule } from  './shared/shared.module';
 import { AppComponent }  from './app.component';
+import { CoreModule }  from './core/core.module';
 
 @NgModule({
-  imports:      [ BrowserModule, CustomersModule, SharedModule ],
+  imports:      [ BrowserModule, CoreModule, CustomersModule, SharedModule ],
   declarations: [ AppComponent ],
   providers: [],
   bootstrap:    [ AppComponent ]
